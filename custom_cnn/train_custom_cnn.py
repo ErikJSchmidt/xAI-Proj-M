45c987ac58ec6f05856a5ea4d697d4a22c44a9ca
erik <erik-jonathan.schmidt@stud.uni-bamberg.de>
2023-11-21 17:10:24 +0100

    .

--- custom_cnn/train_custom_cnn.py
+++ custom_cnn/train_custom_cnn.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import time
 from torchvision.datasets.utils import download_url
 import tarfile
 
@@ -22,7 +21,6 @@ def download_and_unpack_dataset(data_dir):
     dataset_url = "https://s3.amazonaws.com/fast-ai-imageclas/cifar10.tgz"
     download_url(dataset_url, data_dir)
     print("downloaded the compressed dataset")
-    time.sleep(5)
     print("start extracting the dataset")
     # Extract dataset from compressed file
     with tarfile.open(data_dir + '/cifar10.tgz', 'r:gz') as tar:
