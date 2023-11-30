c605c4a391fa1f39986508988c2a3ff09f518939
erik <erik-jonathan.schmidt@stud.uni-bamberg.de>
2023-11-30 19:15:58 +0100

    Added BatchNorm

--- custom_cnn/models.py
+++ custom_cnn/models.py
@@ -116,25 +116,33 @@ class Plain18Layer(ImageClassificationBase):
             nn.ReLU(),
 
             # Conv2:                                                                           Learnable params
-            nn.Conv2d(8,16, kernel_size=3, padding=1, bias=False),      # 8*16*3*3 = 1152
+            nn.Conv2d(8,16, kernel_size=3, padding=1, bias=False),       # 8*16*3*3 = 1152
+            nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.Conv2d(16,16, kernel_size=3, padding=1, bias=False),     # 16*16*3*3 = 2304
+            nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.Conv2d(16,16, kernel_size=3, padding=1, bias=False),     # 16*16*3*3 = 2304
+            nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.Conv2d(16,16, kernel_size=3, padding=1, stride=2, bias=False),     # 16*16*3*3 = 2304
+            nn.BatchNorm2d(16),
             nn.ReLU(),                                                                        # --------------------
                                                                                             # conv2 total = 8064
             # output: 16 x 16 x 16
 
             # Conv3:
             nn.Conv2d(16,32, kernel_size=3, padding=1, bias=False),     # 16*32*3*3 = 4608
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32,32, kernel_size=3, padding=1, bias=False),     # 32*32*3*3 = 9216
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32,32, kernel_size=3, padding=1, bias=False),     # 32*32*3*3 = 9216
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32,32, kernel_size=3, padding=1, stride=2, bias=False),     # 32*32*3*3 = 9216
+            nn.BatchNorm2d(32),
             nn.ReLU(),                                                  # --------------------
             # conv3 total = 32256
 
@@ -142,12 +150,16 @@ class Plain18Layer(ImageClassificationBase):
 
             # Conv4:
             nn.Conv2d(32,64, kernel_size=3, padding=1, bias=False),     # 32*64*3*3 = 18432
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Conv2d(64,64, kernel_size=3, padding=1, bias=False),     # 64*64*3*3 = 36864
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Conv2d(64,64, kernel_size=3, padding=1, bias=False),     # 64*64*3*3 = 36864
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Conv2d(64,64, kernel_size=3, padding=1, stride=2, bias=False),     # 64*64*3*3 = 36864
+            nn.BatchNorm2d(64),
             nn.ReLU(),                                      # --------------------
             # conv4 total = 129024
 
@@ -155,12 +167,16 @@ class Plain18Layer(ImageClassificationBase):
 
             # Conv5:
             nn.Conv2d(64,128, kernel_size=3, padding=1, bias=False),    # 64*128*3*3 = 73728
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1, bias=False),  # 128*128*3*3 = 147456
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1, bias=False),  # 128*128*3*3 = 147456
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1, bias=False),  # 128*128*3*3 = 147456
+            nn.BatchNorm2d(128),
             nn.ReLU(),                                      # --------------------
             # conv5 total = 516696
 
