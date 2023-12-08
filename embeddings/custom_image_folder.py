c972964adf1dacf9f263705e86f753658ceaa895
erik <erik-jonathan.schmidt@stud.uni-bamberg.de>
2023-12-08 15:12:56 +0100

    Scripts for populating a chromadb vector space with ebmeddings from pretrained resnet

---
+++ embeddings/custom_image_folder.py
@@ -0,0 +1,23 @@
+
+from torchvision.datasets import ImageFolder
+from typing import Any, Tuple
+
+class CustomImageFolder(ImageFolder):
+
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return (path, sample), target
