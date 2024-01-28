4d0400546d737ef1d1674abff36e5bcc8bb71069
erik <erik-jonathan.schmidt@stud.uni-bamberg.de>
2024-01-28 17:52:50 +0100

    evaluated the 3 div 3 CE training experiment with 2d embedding space

--- lowdim/evaluation.py
+++ lowdim/evaluation.py
@@ -4,6 +4,10 @@ This file contain function that help to evaluate these artifacts, e.g. calculate
 predictions and the according labels
 """
 import torch
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
 
 def describe_epoch_result(epoch_result_subfolder_path):
     """
@@ -15,21 +19,86 @@ def describe_epoch_result(epoch_result_subfolder_path):
     """
     description = "Describe contents in " + epoch_result_subfolder_path
 
-    for filename in ['train_embeddings', 'train_labels', 'train_predictions', 'val_embeddings', 'val_labels', 'val_predictions']:
+    for filename in ['train_embeddings', 'train_labels', 'train_predictions', 'val_embeddings', 'val_labels',
+                     'val_predictions']:
         data = torch.load(f"{epoch_result_subfolder_path}/{filename}.pt", map_location=torch.device('cpu'))
-        description +=f"\n------{filename}-------"
-        description +=f"\npath: {epoch_result_subfolder_path}/{filename}.pt"
-        description +=f"\ntype of data: {type(data)}"
-        description +=f"\nlength of data: {len(data)}"
-        description +=f"\ntype of elements: {type(data[0])}"
-        description +=f"\nfirst element: {data[0]}"
+        description += f"\n------{filename}-------"
+        description += f"\npath: {epoch_result_subfolder_path}/{filename}.pt"
+        description += f"\ntype of data: {type(data)}"
+        description += f"\nlength of data: {len(data)}"
+        description += f"\ntype of elements: {type(data[0])}"
+        description += f"\nfirst element: {data[0]}"
         try:
-            description +=f"\nlength of single sample: {len(data[0])}"
+            description += f"\nlength of single sample: {len(data[0])}"
         except:
             pass
         try:
-            description +=f"\nshape of single sample: {data[0].shape}"
+            description += f"\nshape of single sample: {data[0].shape}"
         except:
             pass
 
     return description
+
+
+def plot_2d_embeddings(epoch_result_subfolder_path):
+    embedding_filename = "train_embeddings"
+    label_filename = "train_labels"
+    embeddings = torch.load(f"{epoch_result_subfolder_path}/{embedding_filename}.pt", map_location=torch.device('cpu'))
+    labels = torch.load(f"{epoch_result_subfolder_path}/{label_filename}.pt", map_location=torch.device('cpu'))
+
+    embeddings_by_class = []
+    for class_nr in range(0, 10):
+        embeddings_by_class.append([])
+
+    for embedding, class_nr in zip(embeddings, labels):
+        embeddings_by_class[class_nr].append(embedding)
+
+    colors = cm.rainbow(np.linspace(0, 1, len(embeddings_by_class)))
+
+    for class_embeddings, color in list(zip(embeddings_by_class, colors)):
+        x_values = [embedding[0] for embedding in class_embeddings]
+        print(f"max x value:{np.max(x_values)}")
+        y_values = [embedding[1] for embedding in class_embeddings]
+        print(f"max y value:{np.max(y_values)}")
+        plt.scatter(
+            x=x_values,
+            y=y_values,
+            c=color
+        )
+
+    plt.show()
+
+
+def plot_2d_class_centroids(epoch_result_subfolder_path):
+    embedding_filename = "train_embeddings"
+    label_filename = "train_labels"
+    embeddings = torch.load(f"{epoch_result_subfolder_path}/{embedding_filename}.pt", map_location=torch.device('cpu'))
+    labels = torch.load(f"{epoch_result_subfolder_path}/{label_filename}.pt", map_location=torch.device('cpu'))
+
+    embeddings_by_class = []
+    for class_nr in range(0, 10):
+        embeddings_by_class.append([])
+
+    for embedding, class_nr in zip(embeddings, labels):
+        embeddings_by_class[class_nr].append(embedding)
+
+    centroids = []
+    for class_embeddings in embeddings_by_class:
+        x_values = [embedding[0] for embedding in class_embeddings]
+        y_values = [embedding[1] for embedding in class_embeddings]
+
+        x_mean = np.mean(x_values)
+        y_mean = np.mean(y_values)
+
+        centroids.append([x_mean, y_mean])
+
+    colors = cm.rainbow(np.linspace(0, 1, len(embeddings_by_class)))
+
+    for centroid, color in zip(centroids, colors):
+        plt.scatter(
+            x=[centroid[0]],
+            y=[centroid[1]],
+            c=color
+        )
+
+    plt.show()
